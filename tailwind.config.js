7220aae0ccb74ad8af29f9c5707dc2256adb97ee
bernard kusi <bernarddwumfour25@gmail.com>
2024-12-14 06:52:23 +0000

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}', // Ensure all JS and HTML files are included
+  ],
+  theme: {
+    extend: {
+      colors : {
+        primary : "#8837E9",
+        coin : "#F59E0B"
+      }
+    },
+  },
+  plugins: [],
+}
