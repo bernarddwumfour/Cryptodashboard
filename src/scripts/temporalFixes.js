a824fea08586fbb2db35de8e8b2528a7b025ea90
bernard kusi <bernarddwumfour25@gmail.com>
2024-12-16 23:10:41 +0000

    Added withdrawal success and error

---
+++ src/scripts/temporalFixes.js
@@ -0,0 +1,21 @@
+function switchContentOnElementClick(trigger,contentToBeReplaced,replacementcontent){
+    document.querySelector(trigger).addEventListener("click",()=>{
+        document.querySelector(contentToBeReplaced).classList.add("hidden") 
+        document.querySelector(replacementcontent).classList.remove("hidden")
+    })
+}
+function hideContentOnElementClick(trigger,contentToHide){
+    document.querySelector(trigger).addEventListener("click",()=>{
+        document.querySelector(contentToHide).classList.add("hidden")
+    })
+}
+
+//FOR NO TRANSACTION
+switchContentOnElementClick(".overview",".recenttransaction",".norecenttransaction")
+switchContentOnElementClick(".overview",".recenttransactiontable",".norecenttransactiontable")
+hideContentOnElementClick(".overview",".creditCard")
+hideContentOnElementClick(".overview",".withDrawalAmount")
+
+//FOR WITHDRAWAL SUCCESS AND ERROR
+switchContentOnElementClick(".continuetosuccess",".withDrawalAmount",".withdrawalsuccess")
+switchContentOnElementClick(".continuetoerror",".withDrawalAmount",".withdrawalerror")
