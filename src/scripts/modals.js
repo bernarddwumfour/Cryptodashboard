b0a6fe21b790e09b255803123496d4c5bb86b6b8
bernard kusi <bernarddwumfour25@gmail.com>
2024-12-19 02:37:11 +0000

    Added more add coin modals

--- src/scripts/modals.js
+++ src/scripts/modals.js
@@ -2,25 +2,73 @@ let elementsAndEvents = [
   {
     trigger: ".addCoinsButton",
     target: "#addCoinsModal",
-    actions : [
-        {
-            trigger : ".payWithCard",
-            target : "#choosePaymentMethod",
-            actions : [
-                {
-                    trigger : ".continue",
-                    target : "#reviewTransaction"
-                }
-            ]
-        },
-    ]
+    actions: [
+      {
+        trigger: ".payWithCard",
+        target: "#choosePaymentMethod",
+        actions: [
+          {
+            trigger: ".continueToPayment",
+            target: "#reviewTransaction",
+            actions: [
+              {
+                trigger: ".showTransactionErrorButton",
+                target: "#showTransactionErrorModal",
+              },
+              {
+                trigger: ".continueToSpinner",
+                target: "#spinnerModal",
+                actions: [
+                  {
+                    trigger: ".showTransactionSuccessButton",
+                    target: "#showTransactionSuccessModal",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    trigger: ".buyCoinsMultipleOptionsButton",
+    target: "#buyCoinsMultipleOptionsModal",
+    actions: [
+      {
+        trigger: ".payWithCardMultipleCoinOptionsButton",
+        target: "#payWithCardMultipleCoinOptionsModal",
+        actions: [
+          {
+            trigger: ".continueToPayment",
+            target: "#reviewTransaction",
+            actions: [
+              {
+                trigger: ".showTransactionErrorButton",
+                target: "#showTransactionErrorModal",
+              },
+              {
+                trigger: ".continueToSpinner",
+                target: "#spinnerModal",
+                actions: [
+                  {
+                    trigger: ".showTransactionSuccessButton",
+                    target: "#showTransactionSuccessModal",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
 ];
 
 const replaceInnerContent = (container, contentContainer) => {
   let contentToReplace = document.querySelector(contentContainer).innerHTML;
   document.querySelector(container).innerHTML = contentToReplace;
-  console.log(contentToReplace);
+  // console.log(contentToReplace);
   // console.log(document.querySelector(container))
 };
 
@@ -33,28 +81,26 @@ let showModalOnTriggerClick = (elements) => {
         document.querySelector("#modal").classList.add("flex");
         replaceInnerContent("#modalContent", element.target);
 
-
         //Handling All close Modal Events by close button
-        let closeModalButton =
-          document.querySelector("#modal").querySelector(".closeModalButton");
-        closeModalButton.addEventListener("click", () => {
+        let closeModalButton = document
+          .querySelector("#modal")
+          .querySelector(".closeModalButton");
+          closeModalButton && closeModalButton.addEventListener("click", () => {
           document.querySelector("#modal").classList.remove("flex");
           document.querySelector("#modal").classList.add("hidden");
         });
 
-        let cancelModalButton =
-          document.querySelector("#modal").querySelector(".cancelModalButton");
-        cancelModalButton.addEventListener("click", () => {
+        let cancelModalButton = document
+          .querySelector("#modal")
+          .querySelector(".cancelModalButton");
+          cancelModalButton && cancelModalButton.addEventListener("click", () => {
           document.querySelector("#modal").classList.remove("flex");
           document.querySelector("#modal").classList.add("hidden");
         });
 
         //Handle all events("clicks") within the modal
-        console.log(element.actions)
-        element.actions && showModalOnTriggerClick(element.actions)
-        
-
-
+        console.log(element.actions);
+        element.actions && showModalOnTriggerClick(element.actions);
       });
     });
   });
